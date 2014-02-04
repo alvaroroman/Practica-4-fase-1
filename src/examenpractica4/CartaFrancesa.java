98808d635af42f46fff33d2d6b03a3323b2a1127
Alvaro <Alvaro@Alvaro-PC>
2014-02-04 10:01:32 +0100

    Practica4 Fase1

---
+++ src/examenpractica4/CartaFrancesa.java
@@ -0,0 +1,37 @@
+package examenpractica4;
+
+public class CartaFrancesa {
+    public static final int As = 1;
+    public static final int DOS = 2;
+    public static final int TRES = 3;
+    public static final int CUATRO = 4;
+    public static final int CINCO = 5;
+    public static final int SEIS = 6;
+    public static final int SIETE = 7;
+    public static final int OCHO = 8;
+    public static final int NUEVE = 9;
+    public static final int J = 10;
+    public static final int Q = 11;
+    public static final int K = 12;
+    public static final int Treboles = 1;
+    public static final int Diamantes = 2;
+    public static final int Corazones = 3;
+    public static final int Picas = 4;
+    
+    private int palo;
+    private int numero;
+    
+    public int getPalo(){
+        return palo;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public String toString(){
+        
+    }
+
+}
+
